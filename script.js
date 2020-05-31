bf1090c84bdf293eec3ae9754e73ddc4a249bd50
yajaluz <yajaluz@gmail.com>
2020-05-31 16:02:31 -0300

    chat

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const $screen1 = document.querySelector('.screen1');
 
+contNumbers();
 var contNumbers = function(){
-    $(document).on("input", "#TxtObservacoes", function () {
+    $(document).on("input", ".code", function () {
         var limite = 11;
         var caracteresDigitados = $(this).val().length;
         var caracteresRestantes = limite - caracteresDigitados;
