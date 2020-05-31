4d56c1c4d8c9742c9ff00bc039aae7bc7da213a7
yajaluz <yajaluz@gmail.com>
2020-05-31 14:02:05 -0300

    register modified and tradution of message

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const $screen1 = document.querySelector('.screen1');
+
+var contNumbers = function(){
+    $(document).on("input", "#TxtObservacoes", function () {
+        var limite = 11;
+        var caracteresDigitados = $(this).val().length;
+        var caracteresRestantes = limite - caracteresDigitados;
+    
+        $(".caracteres").text(caracteresRestantes);
+    });$(document).on("input", "#TxtObservacoes", function () {
+        var limite = 255;
+        var caracteresDigitados = $(this).val().length;
+        var caracteresRestantes = limite - caracteresDigitados;
+    
+        $(".caracteres").text(caracteresRestantes);
+    });
+
+}
